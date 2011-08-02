2e9bcbaff52ee9c2880889771c96246f63085a1b
Kajetan Rzepecki <kajetan.rzepecki@gmail.com>
2011-08-02 22:39:01 +0200

    Slacking off
    So, I'm apparently still having a little hard time not being lazy.
    Moved rest of the evals and calls to Interpreter. Now it's more generic.
    Fixed Issue 3 with "pass by reference, store by value" policy, will have a
    deeper look into this whenever I feel like.
    Started working on the semantic keywords for function declarator, and I
    figured I could move this one into ASM aswell, but that proves to be
    asspainalistic. I'll try some more before I drop this idea, since it would
    implement neatly in the language AND allow for overrides, making ASM even
    more customisable.
    Finally, did some minor namechanges.

--- imports/core.asm
+++ imports/core.asm
@@ -2,75 +2,83 @@
 # ASM core module.
 ####################
 
+## Some basics:
+
+# Convinient Scope.call.
+(macro __scopecall [arg]
+  `(get '$arg))
+
 ## Convinience macros:
 
 # Function declarator.
 (macro defun [name args body]
-    `(var $name (function $args $body)))
+  `(var $name (function $args $body)))
 
 # Function template declarator.
 (macro template [name targs args body]
-    `(macro $name $targs
-        (function $args $body)))
+  `(macro $name $targs
+    (function $args $body)))
 
 # Class declarator.
 (macro class [name body]
-    (begin (var statics '{})
-           (var locals '{})
-           (defun dispatch [arg]
-               (if (tuple? arg)
-                   (if (equal? 'static (first arg))
-                       (push (rest arg) statics)
-                       (push arg locals))))
-           (map dispatch body)
-           `(var $name
-                 (scope $(join! 'begin (tupleof statics))
-                        (defun new []
-                            (scope $(join! 'begin (tupleof locals))))))))
+  (do (var statics '{})
+      (var locals '{})
+      (defun dispatch [arg]
+        (if (tuple? arg)
+            (if (equal? 'static (first arg))
+                (push (rest arg) statics)
+                (push arg locals))))
+      (map dispatch body)
+      `(var $name
+            (scope $(join! 'do (tupleof statics))
+                   (defun new []
+                     (scope $(join! 'do (tupleof locals))))))))
 
 # Binds variables allowing for lazy evaluation:
 (macro bind [sym obj]
-    (if (in? 'lazy (keywordsof sym))
-        `(var $sym '$obj)
-        `(var $sym $obj)))
+  (if (in? 'lazy (keywordsof sym))
+      `(var $sym '$obj)
+      `(var $sym $obj)))
 
 # Multiple symbol binder:
 (macro alias [object @tuple]
-    (if (rest @tuple)
-        (begin
-            (var 1st `(var $(first @tuple) $object))
-            (defun makeAlias [t] `(var $t $(first @tuple)))
-            (join! 'begin
-                   (join! 1st (map makeAlias (rest @tuple)))))
-    #else `(var $(first @tuple) $object)))
+  (if (rest @tuple)
+      (do (var 1st `(var $(first @tuple) $object))
+          (defun makeAlias [t] `(var $t $(first @tuple)))
+          (join! 'do
+                 (join! 1st (map makeAlias (rest @tuple)))))
+  #else `(var $(first @tuple) $object)))
 
 
 # Boolean operations:
 (macro not [a]
-    `(if $a fnord 'yup))
+  `(if $a fnord 'yup))
 
 (macro and [a b]
-    (begin (var __a a)
-           `(if $__a (if $b $__a))))
+  (do (var __a a)
+      `(if $__a (if $b $__a))))
 
 (macro or [a b]
-    (begin (var __a a)
-           (var __b b)
-           `(if $__a $__a (if $__b $__b))))
+  (do (var __a a)
+      (var __b b)
+      `(if $__a $__a (if $__b $__b))))
 
 # Other macros:
 (macro set [@tuple]  # TODO: Turn it into a function.
-    `'$(setof @tuple))
+  `'$(setof @tuple))
+
 (macro list [@tulpe]
-    `'$(listof @tuple))
+  `'$(listof @tuple))
 
 # Lispy macros:
 
 (macro let* (vars body)
-    `((function []
-        (begin $(join! 'begin
-                       (map (function [pair] (join! 'var pair)) vars))
-               $body))))
+  `((function []
+    (do $(join! 'do
+                (map (function [pair]
+                       (join! 'var pair))
+                     vars))
+        $body))))
 
 (var lambda function)
 
@@ -78,52 +86,69 @@
 
 # Collection manipulation:
 (defun second [c]
-    (first (rest c)))
+  (first (rest c)))
 
 (defun third [c]
-    (first (rest (rest c))))
+  (first (rest (rest c))))
 
 (defun fourth [c]
-    (first (rest (rest (rest c)))))
+  (first (rest (rest (rest c)))))
 
 (defun fifth [c]
-    (first (rest (rest (rest (rest c))))))
+  (first (rest (rest (rest (rest c))))))
 
 (defun in? [el coll]
-    (reduce (function [result arg]
-                (if (fnord? result)
-                    # In case of (in? fnord collection)
-                    (if (equal? arg el) 'yup)
-                    result))
-            (join! fnord coll))) #TODO
+  (reduce (function [result arg]
+            (if (fnord? result)
+                # In case of (in? fnord collection)
+                (if (equal? arg el) 'yup)
+                result))
+          (join! fnord coll))) #TODO
 
-(defun push (what where)
-    (if (settable? where)
-        (set! where (join! what where))))
+(defun push [what where]
+  (if (settable? where)
+      (set! where (join! what where))))
+
+(defun pop [where]
+  (if (settable? (collection? where))
+      (do (var tmp (first where))
+          (set! where (rest where))
+           tmp)))
 
 # Predicates:
 (defun fnord? [obj]
-    (not obj))
+  (not obj))
 
 # Type predicate template:
 (template typePredicate [type] [object]
-    (if (in? $type (typeof object)) object))
+  (if (in? $type (typeof object)) object))
 
-(var immutable? (typePredicate immutable))
-(var settable? (typePredicate settable))
-(var pure? (typePredicate pure))
-(var builtin? (typePredicate builtin))
-(var atom? (typePredicate atom))
-(var callable? (typePredicate callable))
-(var collection? (typePredicate collection))
-(var number? (typePredicate number))
-(var symbol? (typePredicate symbol))
-(var string? (typePredicate string))
-(var function? (typePredicate function))
-(var keyword? (typePredicate keyword))
-(var scope? (typePredicate scope))
-(var set? (typePredicate set))
-(var tuple? (typePredicate tuple))
-(var list? (typePredicate list))
+(macro __definePredicates [predicates]
+  (do (defun makeDecl [arg]
+        `(var $(first arg) (typePredicate $(second arg))))
+      (join! 'do (map makeDecl predicates))))
+
+(__definePredicates
+  ((immutable? immutable)
+  (settable? settable)        # FIXME: This doesn't work as it should.
+  (pure? pure)
+  (builtin? builtin)
+  (atom? atom)
+  (callable? callable)
+  (collection? collection)
+  (number? number)
+  (symbol? symbol)
+  (string? string)
+  (function? function)
+  (keyword? keyword)
+  (scope? scope)
+  (set? set)
+  (tuple? tuple)
+  (list? list)))
 
 # Other functions:
+
+(defun swap [this that]
+  (do (var __this this)
+      (set! this that)
+      (set! that __this)))
