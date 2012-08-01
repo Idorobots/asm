98ec5f74c117a97d9ad375271da4aa7cce386a2c
Kajetan Rzepecki <kajetan.rzepecki@gmail.com>
2012-08-01 15:55:25 +0200

    Added concat-arrow and concat rule.

--- experimental/parser2.asm
+++ experimental/parser2.asm
@@ -1,10 +1,10 @@
 # Parser take two.
 
 # For convinient debugging:
-
 (macro i () '(import 'experimental.parser2))
 
-# Some bindings:
+#######################################################3
+# Some utils:
 
 (var car first)
 (var caar (lambda (lst) (first (first lst))))
@@ -18,8 +18,6 @@
 (var length length?)
 (var cond if*)
 
-(var terminal? string?)
-
 (var *gensym-counter* 0)
 (function gensym (base)
   (do (set! *gensym-counter* (+ 1 *gensym-counter*))
@@ -30,52 +28,68 @@
 ###############################################
 # TODO: Make (tuple string (return)) implicit.
 # TODO: Instead of (Arg <- Rules) use (Rule <- Args) ---> more hygenic.
+# TODO: compile-complex-rule should pass (cdr rule) not rule.
 # FIXME: Strip strings only once!
-# FIXME: Pass stripping? instead of rule type.
 
 ###############################################
-## PEG arrow kinds:
+## PEG parser generator:
 
-(var arrow '<-)
+(var left-arrow '<-)
 (var strip-arrow '<)
-(var append-arrow '<~) # NOTE Currently not implemented.
+(var concat-arrow '<~)
 
-## Syntax macro:
+(macro grammar (@tuple rules)
+  (cons 'do
+        (map (lambda (rule)
+               (make-syntax (car rule) (cdr rule)))
+             rules)))
 
-(macro syntax (rule transform)
+(macro syntax (rule @tuple transform)
+  (make-syntax rule transform))
+
+(function make-syntax (rule transform)
   (do (var name (car rule))
       (var type (cadr rule))
 
-      (unless (member? type (tuple arrow strip-arrow append-arrow))
+      (unless (member? type (tuple left-arrow strip-arrow concat-arrow))
         (error (append "Unrecognized rule type: " (stringof type))))
 
-      (var compiled (compile-rule (cddr rule) type))
+      (var stripping? (equal? type strip-arrow))
+      (var catting? (equal? type concat-arrow))
+
+      (var compiled (if catting?
+                        (compile-rule (tuple '~ (cddr rule)) stripping?)
+                        (compile-rule (cddr rule) stripping?)))
+
       `(function $name (string)
          $(if transform
-              `((lambda ($name) (when $name $transform))
+              `((lambda ($name) (when $name $(append '(do) transform)))
                 ($compiled string))
               `($compiled string)))))
 
-(function compile-rule (rule type)
-  (cond ((terminal? rule) (make-matcher rule (equal? type strip-arrow)))
-        ((tuple? rule) (if (equal? (length rule) 1)
-                           (compile-rule (car rule) type)
-                           (compile-complex-rule rule type)))
-        ('else rule)))
+(function compile-rule (rule stripping?)
+  (cond ((terminal? rule)         (make-matcher rule stripping?))
+        ((nonterminal? rule)      rule)
+        ((equal? (length rule) 1) (compile-rule (car rule) stripping?))
+        ('else                    (compile-complex-rule rule stripping?))))
+
+(var terminal? string?)
+(var nonterminal? symbol?) # FIXME Accepts strings.
 
 (function make-matcher (term stripping?)
-  `(lambda (string)
-     (do (var str $(if stripping?
-                       '(strip string)
-                       'string))
-         (var match (car (regex-match $(append "^" term)
-                                      str)))
-         (when match
-           (tuple (advance str (length match))
-                  (tuple match))))))
+  (do (var str (if stripping? 'str 'string))
+      (var match (gensym "match"))
+      `(lambda (string)
+         (do $(when stripping?
+                '(var str (strip string)))
+             (var $match (car (regex-match $(append "^" term)
+                                          $str)))
+             (when $match
+               (tuple (advance $str (length $match))
+                      (tuple $match)))))))
 
 (function strip (string)
-  (if (var (stripped skip) (Space string))
+  (if (var (stripped skip) (Spacing string))
       stripped
       string))
 
@@ -84,126 +98,140 @@
       collection
       (advance (cdr collection) (- n 1))))
 
-(function compile-complex-rule (rule type)
+(function compile-complex-rule (rule stripping?)
   (do (var fst (car rule))
-      (cond ((equal? fst '/) (compile-or rule type))
-            ((equal? fst '*) (compile-zero-or-more rule type))
-            ((equal? fst '+) (compile-one-or-more rule type))
-            ((equal? fst '?) (compile-optional rule type))
-            ((equal? fst '!) (compile-not rule type))
-            ((equal? fst '&) (compile-and rule type))
-            ((equal? fst ':) (compile-drop rule type))
-            ('else (compile-sequence rule type)))))
+      (cond ((equal? fst '/) (compile-or rule stripping?))
+            ((equal? fst '*) (compile-zero-or-more rule stripping?))
+            ((equal? fst '+) (compile-one-or-more rule stripping?))
+            ((equal? fst ':) (compile-drop rule stripping?))
+            ((equal? fst '?) (compile-optional rule stripping?))
+            ((equal? fst '!) (compile-not rule stripping?))
+            ((equal? fst '&) (compile-and rule stripping?))
+            ((equal? fst '~) (compile-concat rule stripping?))
+            ('else (compile-sequence rule stripping?)))))
 
 # (...)
-(function compile-sequence (rules type)
+(function compile-sequence (rules stripping?)
   `(lambda (string)
-     $(compile-sequence-each rules type 'string ())))
+     $(compile-sequence-each rules stripping? 'string ())))
 
-(function compile-sequence-each (rules type last-str names)
+(function compile-sequence-each (rules stripping? last-str names)
   (if rules
-      (do (var stripping? (equal? type strip-arrow))
-          (var str (gensym "str"))
+      (do (var str (gensym "str"))
           (var match (gensym "match"))
-          `(when (var ($str $match) ($(compile-rule (car rules) type) $(if stripping?
-                                                                           (tuple 'strip last-str)
-                                                                           last-str)))
-             $(compile-sequence-each (cdr rules) type str (append names (tuple match)))))
+          `(when (var ($str $match) ($(compile-rule (car rules) stripping?)
+                                     $(if stripping?
+                                          (tuple 'strip last-str)
+                                          last-str)))
+             $(compile-sequence-each (cdr rules) stripping? str (append names (tuple match)))))
       (tuple 'tuple last-str (cons 'append names))))
 
 # (/ ...)
-(function compile-or (rules type)
-  (do (var stripping? (equal? type strip-arrow))
-      `(lambda (string)
-         (do (var str $(if stripping?
-                           '(strip string)
-                           'string))
-             $(compile-or-each (cdr rules) type)))))
+(function compile-or (rules stripping?)
+  `(lambda (string)
+     (do (var str $(if stripping?
+                       '(strip string)
+                       'string))
+         $(compile-or-each (cdr rules) stripping?))))
 
-(function compile-or-each (rules type)
+(function compile-or-each (rules stripping?)
   (when rules
     (var match (gensym "match"))
-    `(if (var $match ($(compile-rule (car rules) type) str))
+    `(if (var $match ($(compile-rule (car rules) stripping?) str))
          $match
-         $(compile-or-each (cdr rules) type))))
+         $(compile-or-each (cdr rules) stripping?))))
 
 # (* ...)
-(function compile-zero-or-more (rule type)
-  (do (var stripping? (equal? type strip-arrow))
-      `(lambda (str)
-         (do (function zero-or-more (string matches)
-               (if (var (munched match) ($(compile-rule (cdr rule) type) $(if stripping?
-                                                                              '(strip string)
-                                                                              'string)))
-                   (zero-or-more munched (append matches match))
-                   (if matches
-                       (tuple string matches)
-                       (tuple string)))) # A null is fine too.
-             (zero-or-more str ())))))
+(function compile-zero-or-more (rule stripping?)
+  `(lambda (str)
+     (do (function zero-or-more (string matches)
+           (if (var (munched match) ($(compile-rule (cdr rule) stripping?)
+                                     $(if stripping?
+                                          '(strip string)
+                                          'string)))
+               (zero-or-more munched (append matches match))
+               (if matches
+                   (tuple string matches)
+                   (tuple string)))) # A null is fine too.
+         (zero-or-more str ()))))
 
 # (+ ...)
-(function compile-one-or-more (rule type)
-  (do (var stripping? (equal? type strip-arrow))
-      `(lambda (str)
-         (do (function one-or-more (string matches)
-               (if (var (munched match) ($(compile-rule (cdr rule) type) $(if stripping?
-                                                                              '(strip string)
-                                                                              'string)))
-                   (one-or-more munched (append matches match))
-                   (when matches
-                     (tuple string matches))))
-             (one-or-more str ())))))
+(function compile-one-or-more (rule stripping?)
+  `(lambda (str)
+     (do (function one-or-more (string matches)
+           (if (var (munched match) ($(compile-rule (cdr rule) stripping?) $(if stripping?
+                                                                               '(strip string)
+                                                                               'string)))
+               (one-or-more munched (append matches match))
+               (when matches
+                 (tuple string matches))))
+         (one-or-more str ()))))
 
 # (? ...)
-(function compile-optional (rule type)
+(function compile-optional (rule stripping?)
   (do (var foo (gensym "foo"))
       `(lambda (string)
-         (if (var $foo ($(compile-rule (cdr rule) type) string))
+         (if (var $foo ($(compile-rule (cdr rule) stripping?) string))
              $foo
              (tuple string)))))
 
 # (! ...)
-(function compile-not (rule type)
+(function compile-not (rule stripping?)
   `(lambda (string)
-     (unless ($(compile-rule (cdr rule) type) string)
+     (unless ($(compile-rule (cdr rule) stripping?) string)
        (tuple string))))
 
 # (& ...)
-(function compile-and (rule type)
+(function compile-and (rule stripping?)
   `(lambda (string)
-     (when ($(compile-rule (cdr rule) type) string)
+     (when ($(compile-rule (cdr rule) stripping?) string)
        (tuple string))))
 
 # (: ...)
-(function compile-drop (rule type)
+(function compile-drop (rule stripping?)
    (do (var str (gensym "str"))
        (var match (gensym "match"))
        `(lambda (string)
-          (when (var ($str $match) ($(compile-rule (cdr rule) type) string))
+          (when (var ($str $match) ($(compile-rule (cdr rule) stripping?) string))
             (tuple $str)))))
 
-#### Simple Lisp:
-(syntax (Space <- (/ Comment "[\\s]+")) ())
+# (~ ...)
+(function compile-concat (rule stripping?)
+  (do (var str (gensym "str"))
+      (var match (gensym "match"))
+      `(lambda (string)
+         (when (var ($str $match) ($(compile-rule (cdr rule) stripping?) string))
+           (tuple $str
+                  (tuple (reduce append $match "")))))))
 
-(syntax (Comment <- (: "#" "[^\\n]*\\n")) ())
+##############################################
+## REPL:
 
-(syntax (Expression < (/ String Atom List)) ())
+(var abort? "abort")
+(var current-scope (vau (env) env))
+(var *global-scope* (current-scope))
 
-(syntax (String <- (:"\"") "[^\"]*" (:"\"")) ())
+(function repl ()
+  (do (write "# Experimental REPL. You can exit at any time by typing `" abort? "'.")
+      (read-eval-print-loop 0 *global-scope*)
+      (write "# Have a nice day!")))
 
-(syntax (Atom <- (/ Number Symbol)) ())
-
-(syntax (Number <- "[+\\-]?[0-9]+(\\.[0-9]*)?")
-  `($(car Number)
-    ($(str->num (caadr Number)))))
-
-(syntax (Symbol <- (! Number) "[^\\(\\)\\[\\]\\{\\}'`\\$#\\s]+")
-  `($(car Symbol)
-    ($(str->symbol (caadr Symbol)))))
-
-(syntax (List < (: "\\(") (* Expression) (: "\\)"))
-  `($(car List)
-    $(cdr List)))
+(function read-eval-print-loop (counter env)
+  (unless (equal? counter -1)
+    (if (equal? counter 0)
+        (write "\n# ")
+        (write "\n" counter "# "))
+    (if (equal? (var input (readln)) abort?)
+        (set! counter -1) # Abort! Abort!
+        (catch (write "\t " (eval (parse input) env))
+               (lambda (e)
+                 (do (write "Cought an exception in " (if (equal? counter 0)
+                                                          "main"
+                                                          "the")
+                            " loop:\n" e "\n")
+                     (write "\nYou can abort the execution by typing `" abort? "'.\n")
+                     (read-eval-print-loop (+ counter 1) (scope "New scope."))))))
+    (read-eval-print-loop counter env)))
 
 (function parse (string)
   (do (var (rest parse) (Expression string))
@@ -211,33 +239,48 @@
            ((not parse) (error (append "Parsing error: Shit doesn't parse: `" string "'.")))
            ('else (car parse)))))
 
-##############################################
-## The REPL:
+#########################################################
+# Default parsers:
 
-(var abort? "abort")
-(var current-scope (vau (env) env))
-(var *global-scope* (current-scope))
+(grammar ((Alpha       <- "[a-zA-Z_]"))
+         ((Digit       <- "[0-9]"))
+         ((Alphanum    <- (/ Alpha Digit)))
+         ((Identifier  <~ Alpha (* Alphanum)))
+         ((Space       <- " "))
+         ((Blank       <- (/ Space "\t" "\v")))
+         ((LF          <- "\n"))
+         ((CR          <- "\r"))
+         ((CRLF        <- "\r\n"))
+         ((EOL         <- (/ CRLF LF CR)))
+         ((EOI         <- ""))
+         ((Spacing     <- (* (/ Blank EOL))))
+         ((DoubleQuote <- "\""))
+         ((Quote       <- "'"))
+         ((BackQuote   <- "`"))
+         ((Slash       <- "/"))
+         ((BackSlash   <- "\\"))
+         ((Line        <~ (* (! EOL) ".") (/ EOL EOI)))
+         ((Lines       <- (+ Line))))
 
-(function read-eval-print-loop (counter env)
-  (do @until (equal? counter -1)
-      (if (equal? counter 0)
-          (write "\n# ")
-          (write "\n" counter "# "))
-      (if (equal? (var input (readln)) abort?)
-          (set! counter -1) # Abort! Abort!
-          (catch (write "\t " (eval (parse input) env))
-                 (lambda (e)
-                   (do (write "Cought an exception in " (if (equal? counter 0)
-                                                            "main"
-                                                            "the")
-                              " loop:\n" e "\n")
-                       (write "\nYou can abort the execution by typing `" abort? "'.\n")
-                       (read-eval-print-loop (+ counter 1) (scope "New scope."))))))))
+"\"" # FIXME Old parser bug, lol.
 
-(function repl ()
-  (do (write "# Experimental REPL. You can exit at any time by typing `" abort? "'.")
-      (read-eval-print-loop 0 *global-scope*)
-      (write "# Have a nice day!")))
+##########################################################
+## Simple Lisp:
+
+(grammar ((Expression < (/ String List Atom)))
+         ((String     <- (:"\"") "[^\"]*" (: "\"")))
+         ((List       < (: "\\(") (* Expression) (: "\\)"))
+                        `($(car List)
+                          $(cdr List)))
+         ((Atom       <- (/ Number Symbol)))
+         ((Number     <- "[+\\-]?[0-9]+(\\.[0-9]*)?")
+                         `($(car Number)
+                           ($(str->num (caadr Number)))))
+         ((Symbol     <- (! Number) "[^\\(\\)\\[\\]\\{\\}\"'`\\$#\\s]+")
+                         `($(car Symbol)
+                           ($(str->symbol (caadr Symbol)))))
+         ((Spacing    <- (* (/ Comment "[\\s]+"))))
+         ((Comment    <- (: "#" "[^\n]*\n"))))
 
 #########################################
 #### Some macros:
@@ -267,53 +310,51 @@
        $(cadr t)
        $(caddr t)))
 
+#############################################
 # JSON anybody?
-(syntax (JValue < (/ String Number JObject JArray JTrue JFalse JNull)) ())
 
-(syntax (JObject < (: "\\{") (? JPair (* (:",") JPair)) (:"\\}"))
-  `($(car JObject)
-    ($(cons 'scope (cadr JObject)))))
-
-(syntax (JPair < String (: ":") JValue)
-  `($(car JPair)
-    ((var $(str->symbol (caadr JPair)) $(cadadr JPair)))))
-
-(syntax (JArray < (:"\\[") (? JValue (* (:",") JValue)) (:"\\]"))
-  `($(car JArray)
-    ($(vectorof (cadr JArray)))))
-
-(syntax (JTrue <- "true") ())
-(syntax (JFalse <- "false") ())
-(syntax (JNull <- "null") ())
+(grammar ((JValue  < (/ String Number JObject JArray JTrue JFalse JNull)))
+         ((JObject < (: "\\{") (? JPair (* (:",") JPair)) (:"\\}"))
+                     `($(car JObject)
+                       ($(cons 'scope (cadr JObject)))))
+         ((JPair   < String (: ":") JValue)
+                     `($(car JPair)
+                       ((var $(str->symbol (caadr JPair)) $(cadadr JPair)))))
+         ((JArray  < (:"\\[") (? JValue (* (:",") JValue)) (:"\\]"))
+                     `($(car JArray)
+                        ($(vectorof (cadr JArray)))))
+         ((JTrue   <- (: "true"))
+                      `($(car JTrue)
+                         ('t)))
+         ((JFalse  <- (: "false")))
+         ((JNull   <- (: "null"))))
 
 ## Add it to the Lispy grammar:
-(syntax (Expression < (/ Quote JObject String List Atom)) ())
+(syntax (Expression < (/ Quote JObject String List Atom)))
+
+##############################################
+# Semantic comments:
+
+(grammar ((Comment     <- (/ InfoComment MetaComment ExprComment LineComment)))
+         ((InfoComment <- (: "#\\?") String)
+                          (do (warning (caadr InfoComment))
+                              (tuple (car InfoComment))))
+         ((ExprComment <- (:"#\\+" Expression)))
+         ((MetaComment <- (: "#!") Expression)
+                          (do (eval (caadr MetaComment))
+                              (tuple (car MetaComment))))
+         ((LineComment <- (: "#" "[^\\n]*\\n"))))
 
 # Some tests:
-(JObject "{
-  \"Number\": 42,
+(write (JObject "{
+  \"Number\" : 42,
   \"Decimal\": 123.456,
-  \"String\": \"abc\",
-  \"Empty\" : {},
-  \"Array\" : [0,1,2],
-  \"Array2\": [0, [0,1,2], \"abc\"],
-  \"Obj\"   : { \"Member\":0, \"Member\":[0,1,2] },
-  \"True\"  : true,
-  \"False\" : false,
-  \"Null\"  : null
-}")
-
-# Semantic comments:
-(syntax (Comment <- (/ InfoComment MetaComment ExprComment LineComment)) ())
-
-(syntax (InfoComment <- (: "#\\?") String)
-  (do (warn (caadr InfoComment))
-      (tuple (car InfoComment))))
-
-(syntax (LineComment <- (: "#" "[^\\n]*\\n")) ())
-
-(syntax (ExprComment <- (:"#\\+" Expression)) ())
-
-(syntax (MetaComment <- (: "#!") Expression)
-  (do (eval (caadr MetaComment))
-      (tuple (car MetaComment))))
+  \"String\" : \"abc\", #?\"This is a string.\"
+  \"Empty\"  : {},
+  \"Array\"  : [0,1,2],
+  \"Array2\" : [0, [0,1,2], \"abc\"],
+  \"Obj\"    : { \"Member\":0, \"Member\":[0,1,2] },
+  \"True\"   : true,
+  \"False\"  : false,
+  \"Null\"   : null
+}"))
